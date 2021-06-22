f089b7d90dec0b8cd72e5a04bbfe0bbb47b067a5
GitHub Action <action@github.com>
2021-06-22 22:53:02 +0000

    Template cleanup

--- src/main/kotlin/com/github/actualkwarter/yoshisislandprogressbar/MyBundle.kt
+++ src/main/kotlin/com/github/actualkwarter/yoshisislandprogressbar/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.actualkwarter.yoshisislandprogressbar
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/actualkwarter/yoshisislandprogressbar/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.actualkwarter.yoshisislandprogressbar.services
+
+import com.github.actualkwarter.yoshisislandprogressbar.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
